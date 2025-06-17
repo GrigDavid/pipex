1b34ffc68cd38cb2a23b22deb1ffa3664c347613
Davit Grigoryan <dgrigor2@c2r10s6.42yerevan.am>
2025-06-17 14:37:32 +0400

    jhgsfhgsdk

--- pipex.c
+++ pipex.c
@@ -1,36 +1,23 @@
 #include "pipex.h"
 
-int	main(void)
+int	validate(int argc, char ** argv, char **envp)
 {
-	int	id;
-	int	fd[2];
-	//int	input;
-	char	*tmp;
-
-	tmp = (char *)malloc(100);
-	if (!tmp)
-		return (1);
-	if (pipe(fd) == -1)
-		return (1);
-	id = fork();
-	if (id == 0)
-	{
-		close(fd[0]);
-		write(1, "\ninput a number: ", 17);
-		while (read(0, tmp, 100) == 0)
-		{}
-		if (!tmp)
-			return (1);
-		write(fd[1], tmp, sizeof(int));
-		close(fd[1]);
-	}
+	if (argc != 5)//check argc
+		return (0);
+	if (!access(argv[1], F_OK))//check file1 existance
+		if (access(argv[1], R_OK))//check file1 readability
+			return (0);
 	else
+			return(0);
+	if (access(set_to_path(argv[2]), F_OK))
+
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (!validate(argc, argv, envp))
 	{
-		wait(NULL);
-		close(fd[1]);
-		write(1, "\ngot from your input: ", 22);
-		read (fd[0], tmp, sizeof(int));
-		printf("%s\n", tmp);
-		close(fd[0]);
+		return (1);
 	}
+
 }
