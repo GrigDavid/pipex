ecf4cf73f4a2b5a620c32d1ab1d37000bb4d1163
Davit Grigoryan <dgrigor2@c2r11s5.42yerevan.am>
2025-06-28 15:01:01 +0400

    emptying file before writing to it

--- pipex.c
+++ pipex.c
@@ -8,14 +8,16 @@ void	x_cmd(int fd_in, int fd_out, char *name, char **envp)
 	arg = (char **)malloc(sizeof(char *) * 2);
 	if (!arg)
 		return ;
-	arg[0] = name;
-	arg[1] = NULL;
+	arg = ft_split(name, ' ');
+	if (!arg)
+		return ;
 	dup2(fd_in, STDIN_FILENO);
 	dup2(fd_out, STDOUT_FILENO);
 	close(fd_in);
+	close(fd_out);
 	path = set_to_path(envp, name);
 	if (!path)
-		return ;
+		return ;	
 	execve(path, arg, envp);
 }
 
@@ -29,15 +31,19 @@ void	kanchox_zibil(char **argv, char **envp)
 	if (pipe(p) == -1)
 		return ;
 	pid = fork();
+	if (pid < 0)
+		return ;
 	if (pid == 0)
 	{
 		file1_fd = open(argv[1], O_RDONLY);
-		if (file1_fd == -1)
+		if (file1_fd < 0)
 			return ;
+		close(p[0]);
 		x_cmd(file1_fd, p[1], argv[2], envp);
 	}
+	waitpid(pid, NULL, 0);
 	close(p[1]);
-	file2_fd = open(argv[4], O_WRONLY);
+	file2_fd = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0777); // turn this into if statement so if the file does not exist than on error it should be deleted using unlink()
 	if (file2_fd == -1)
 		return ;
 	x_cmd(p[0], file2_fd, argv[3], envp);
--- src/set_to_path.c
+++ src/set_to_path.c
@@ -13,6 +13,34 @@ static void	ft_free_mat(char **arr)
 	free(arr);
 }
 
+static char	*ft_strjoin_chr(char *str, char c, char *dst)
+{
+	int	i;
+	int	len;
+	char	*res;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			break;
+		i++;
+	}
+	len = ft_strlen(dst);
+	res = (char *)malloc((i + len + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	res[i-- + len] = 0;
+	while (i >= 0)
+	{
+		res[i + len] = str[i];
+		i--;
+	}
+	while (len-- > 0)
+		res[len] = dst[len];
+	return (res);
+}
+
 static int	check_prog(char *file)
 {
 	if (!access(file, F_OK) && !access(file, X_OK))
@@ -44,7 +72,7 @@ char	*set_to_path(char **envp, char *file)
 		return(ft_free_mat(paths), NULL);
 	while (paths[i])
 	{
-		tmp = ft_strjoin(paths[i++], file);
+		tmp = ft_strjoin_chr(file, ' ', paths[i++]);
 		if (!tmp)
 			return (free(file), ft_free_mat(paths), NULL);
 		if (check_prog(tmp))
